a57c070d7e4ea4953db0ddbf76869eb185212ff7
echenini <echeninidavid@gmail.com>
2025-09-19 09:04:49 +0100

    Update heading.php

--- heading.php
+++ heading.php
@@ -1,41 +1,38 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Untitled Document</title>
-<link rel="stylesheet" href="style.css">
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
-</head>
-<body>
+<!-- heading.php -->
 <table width="1000" height="200" border="0" align="center" bgcolor="#dfe2e7">
-  <tr  bgcolor="#dfe2e7">
-    <td width="200"><img src="Images/immacu.png" width="200" height="200" /></td>
-    <td width="1156"align="center"  bgcolor="#dfe2e7"><p class="style1"><div class="imm"><span class="style2 style12 style11">Immaculate</span> <span class="style10 style11">College  </span></p></div>
-    <marquee>Welcome to Immacculate College's Portal</marquee></td>
+  <tr bgcolor="#dfe2e7">
+    <td width="200">
+      <img src="Images/immacu.png" width="200" height="200" alt="Immaculate College Logo" />
+    </td>
+    <td width="1156" align="center" bgcolor="#dfe2e7">
+      <div class="imm">
+        <span class="style2 style12 style11">Immaculate</span>
+        <span class="style10 style11">College</span>
+      </div>
+      <marquee>Welcome to Immacculate College's Portal</marquee>
+    </td>
   </tr>
 </table> 
 
- <ul class="lag">
-        <li class="iod" style="--i:#1da1f2; --j:#e5683d">
-            <i class="fa-solid fa-house fa-lg"></i>
-            <span class="title"><a href="index.php">Home</a></span>
-        </li>
-       <li  class="iod" style="--i:#1da1f2; --j:#e5683d">
-            <i class="fa-solid fa-users fa-lg"></i>
-            <span class="title"><a href="about.php">AboutUs</a></span>
-        </li>
-       <li  class="iod" style="--i:#1da1f2; --j:#e5683d">
-            <i class="fa-solid fa-phone fa-lg"></i>
-            <span class="title"><a href="contact.php">ContactUs</a></span>
-        </li>
-        <li  class="iod" style="--i:#1da1f2; --j:#e5683d">
-            <i class="fa-solid fa-gear fa-lg"></i>
-            <span class="title"><a href="serve.php">Services</a></span>
-        </li>
-       <li  class="iod" style="--i:#1da1f2; --j:#e5683d">
+<ul class="lag">
+  <li class="iod" style="--i:#1da1f2; --j:#e5683d">
+    <i class="fa-solid fa-house fa-lg"></i>
+    <span class="title"><a href="index.php">Home</a></span>
+  </li>
+  <li class="iod" style="--i:#1da1f2; --j:#e5683d">
+    <i class="fa-solid fa-users fa-lg"></i>
+    <span class="title"><a href="about.php">About Us</a></span>
+  </li>
+  <li class="iod" style="--i:#1da1f2; --j:#e5683d">
+    <i class="fa-solid fa-phone fa-lg"></i>
+    <span class="title"><a href="contact.php">Contact Us</a></span>
+  </li>
+  <li class="iod" style="--i:#1da1f2; --j:#e5683d">
+    <i class="fa-solid fa-gear fa-lg"></i>
+    <span class="title"><a href="serve.php">Services</a></span>
+  </li>
+  <li class="iod" style="--i:#1da1f2; --j:#e5683d">
     <i class="fa-solid fa-right-to-bracket fa-lg"></i>
     <span class="title"><a href="login.php">Login</a></span>
-</li>
-    </ul>
-</body>
-</html>
+  </li>
+</ul>
